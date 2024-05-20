74f7b9fda8bdaf9164205f5e6d96db268e25eeed
ray <audi3m@naver.com>
2024-05-21 00:16:03 +0900

    Initial Commit

---
+++ HW0520second/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW0520second
+//
+//  Created by J Oh on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
